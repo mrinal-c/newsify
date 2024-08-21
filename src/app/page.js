8d6c8c52c76e15e6d3ca9769dae57c68fb1fd368
Mrinal <cmrinal16@gmail.com>
2024-08-21 17:42:48 -0400

    set up auth and middleware

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,46 @@
 import { auth, signIn } from "@/auth";
+import "@/app/page.css";
+import spotifyLogo from "../../public/spotify.png";
 
 export default async function Page() {
-  const session = await auth();
-  if (!session)
-    return (
-      <div>
-        Not authenticated
+  // const session = await auth();
+  // if (!session)
+  //   return (
+  //     <div>
+  //       Not authenticated
+  // <form
+  //   action={async () => {
+  //     "use server";
+  //     await signIn();
+  //   }}
+  // >
+  //   <button type="submit">Sign in</button>
+  // </form>
+  //     </div>
+  //   );
+
+  // return (
+  //   <div>
+  //     <pre>{JSON.stringify(session, null, 2)}</pre>
+  //   </div>
+  // );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to Newsify!</p>
         <form
           action={async () => {
             "use server";
-            await signIn();
+            await signIn('spotify',{redirectTo: '/dashboard'});
           }}
         >
-          <button type="submit">Sign in</button>
+          <button type="submit" className="login-btn">
+            <img src={spotifyLogo} alt="Spotify Logo" className="logo" />
+            Login with Spotify
+          </button>
         </form>
-      </div>
-    );
-
-  return (
-    <div>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      </header>
     </div>
   );
 }
--- src/middleware.js
+++ src/middleware.js
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/") {
+    return Response.redirect(new URL("/", req.nextUrl.origin));
+  } else if (req.auth && req.nextUrl.pathname === "/") {
+    return Response.redirect(new URL("/dashboard", req.nextUrl.origin));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
